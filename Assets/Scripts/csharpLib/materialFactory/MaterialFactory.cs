7d899c971d015918ee381fe3fd96bc9a78d621e3
ludi <ludidilu@163.com>
2018-02-02 15:52:07 +0800

    a

---
+++ Assets/Scripts/csharpLib/materialFactory/MaterialFactory.cs
@@ -0,0 +1,92 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System;
+
+namespace materialFactory
+{
+    public class MaterialFactory
+    {
+        public Dictionary<string, MaterialFactoryUnit> dic = new Dictionary<string, MaterialFactoryUnit>();
+
+        private static MaterialFactory _Instance;
+
+        public static MaterialFactory Instance
+        {
+            get
+            {
+                if (_Instance == null)
+                {
+                    _Instance = new MaterialFactory();
+                }
+
+                return _Instance;
+            }
+        }
+
+        public Material GetMaterial(string _path, Action<Material> _callBack)
+        {
+            MaterialFactoryUnit unit;
+
+            if (!dic.TryGetValue(_path, out unit))
+            {
+                unit = new MaterialFactoryUnit(_path);
+
+                dic.Add(_path, unit);
+            }
+
+            return unit.GetMaterial(_callBack);
+        }
+
+        public void AddUseNum(string _path)
+        {
+            MaterialFactoryUnit unit;
+
+            if (dic.TryGetValue(_path, out unit))
+            {
+                unit.AddUseNum();
+            }
+        }
+
+        public void DelUseNum(string _path)
+        {
+            MaterialFactoryUnit unit;
+
+            if (dic.TryGetValue(_path, out unit))
+            {
+                unit.DelUseNum();
+            }
+        }
+
+        public void Dispose(bool _force)
+        {
+            List<string> delKeyList = null;
+
+            IEnumerator<KeyValuePair<string, MaterialFactoryUnit>> enumerator = dic.GetEnumerator();
+
+            while (enumerator.MoveNext())
+            {
+                KeyValuePair<string, MaterialFactoryUnit> pair = enumerator.Current;
+
+                if (_force || pair.Value.useNum == 0)
+                {
+                    pair.Value.Dispose();
+
+                    if (delKeyList == null)
+                    {
+                        delKeyList = new List<string>();
+                    }
+
+                    delKeyList.Add(pair.Key);
+                }
+            }
+
+            if (delKeyList != null)
+            {
+                for (int i = 0; i < delKeyList.Count; i++)
+                {
+                    dic.Remove(delKeyList[i]);
+                }
+            }
+        }
+    }
+}
--- Assets/Scripts/csharpLib/materialFactory/MaterialFactoryUnit.cs
+++ Assets/Scripts/csharpLib/materialFactory/MaterialFactoryUnit.cs
@@ -3,25 +3,25 @@ using System.Collections.Generic;
 using System;
 using assetManager;
 
-namespace animatorFactoty
+namespace materialFactory
 {
-    public class AnimatorFactoryUnit
+    public class MaterialFactoryUnit
     {
         private string name;
-        private RuntimeAnimatorController data;
+        private Material data;
 
         private int type = -1;
 
-        private List<Action<RuntimeAnimatorController>> callBackList = new List<Action<RuntimeAnimatorController>>();
+        private List<Action<Material>> callBackList = new List<Action<Material>>();
 
         public int useNum;
 
-        public AnimatorFactoryUnit(string _name)
+        public MaterialFactoryUnit(string _name)
         {
             name = _name;
         }
 
-        public RuntimeAnimatorController GetAnimator(Action<RuntimeAnimatorController> _callBack)
+        public Material GetMaterial(Action<Material> _callBack)
         {
             if (type == -1)
             {
@@ -29,7 +29,7 @@ namespace animatorFactoty
 
                 callBackList.Add(_callBack);
 
-                AssetManager.Instance.GetAsset<RuntimeAnimatorController>(name, GetAsset);
+                AssetManager.Instance.GetAsset<Material>(name, GetAsset);
 
                 return null;
             }
@@ -50,11 +50,9 @@ namespace animatorFactoty
             }
         }
 
-        private void GetAsset(RuntimeAnimatorController _data)
+        private void GetAsset(Material _data)
         {
             data = _data;
-
-            data.name = name;
 
             type = 1;
 
