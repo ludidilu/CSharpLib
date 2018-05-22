4505677c4fee01d7f30cbe282955cf79e5822350
ludidilu <ludidilu@163.com>
2018-05-22 11:49:30 +0800

    a

--- Assets/Scripts/csharpLib/gameObjectFactory/GameObjectFactoryUnit.cs
+++ Assets/Scripts/csharpLib/gameObjectFactory/GameObjectFactoryUnit.cs
@@ -6,80 +6,80 @@ using System;
 namespace gameObjectFactory
 {
     public class GameObjectFactoryUnit
-	{
-		private string name;
+    {
+        private string name;
 
-		private GameObject data;
+        private GameObject data;
 
-		private int type = -1;
+        private int type = -1;
 
-		public int useNum{private set;get;}
+        public int useNum { private set; get; }
 
-		private List<Action<GameObject>> callBackList = new List<Action<GameObject>>();
+        private List<Action<GameObject>> callBackList = new List<Action<GameObject>>();
 
-		private List<Action> callBackList2 = new List<Action>();
+        private List<Action> callBackList2 = new List<Action>();
 
-		public GameObjectFactoryUnit (string _name)
-		{
-			name = _name;
-		}
+        public GameObjectFactoryUnit(string _name)
+        {
+            name = _name;
+        }
 
-        public void GetGameObject (Action _callBack){
-			
-			if (type == -1) {
-				
-				type = 0;
-				
-				callBackList2.Add (_callBack);
-				
-				AssetManager.Instance.GetAsset<GameObject> (name, GetResouece);
-				
-			} else if (type == 0) {
-				
-				callBackList2.Add (_callBack);
-				
-			} else {
-				
-				_callBack ();
-			}
-		}
+        public void GetGameObject(Action _callBack)
+        {
+            if (type == -1)
+            {
+                type = 0;
 
-		public GameObject GetGameObject (Action<GameObject> _callBack){
+                callBackList2.Add(_callBack);
 
-			if (type == -1) {
+                AssetManager.Instance.GetAsset<GameObject>(name, GetResouece);
+            }
+            else if (type == 0)
+            {
+                callBackList2.Add(_callBack);
+            }
+            else
+            {
+                _callBack();
+            }
+        }
 
-				type = 0;
+        public GameObject GetGameObject(Action<GameObject> _callBack)
+        {
+            if (type == -1)
+            {
+                type = 0;
 
-				callBackList.Add (_callBack);
+                callBackList.Add(_callBack);
 
-				AssetManager.Instance.GetAsset<GameObject> (name, GetResouece);
+                AssetManager.Instance.GetAsset<GameObject>(name, GetResouece);
 
-				return null;
+                return null;
+            }
+            else if (type == 0)
+            {
+                callBackList.Add(_callBack);
 
-			} else if (type == 0) {
+                return null;
+            }
+            else
+            {
+                GameObject result = GameObject.Instantiate<GameObject>(data);
 
-				callBackList.Add (_callBack);
+                if (_callBack != null)
+                {
+                    _callBack(result);
+                }
 
-				return null;
+                return result;
+            }
+        }
 
-			} else {
+        private void GetResouece(GameObject _go)
+        {
+            data = _go;
 
-				GameObject result = GameObject.Instantiate<GameObject> (data);
-
-				if (_callBack != null) {
-
-					_callBack (result);
-				}
-
-				return result;
-			}
-		}
-
-		private void GetResouece (GameObject _go)
-		{
-			data = _go;
-
-			type = 1;
+            type = 1;
 
             for (int i = 0; i < callBackList.Count; i++)
             {
@@ -93,14 +93,15 @@ namespace gameObjectFactory
 
                         callBack(result);
                     }
-                    else {
+                    else
+                    {
 
                         callBack(null);
                     }
                 }
             }
 
-			callBackList.Clear ();
+            callBackList.Clear();
 
             for (int i = 0; i < callBackList2.Count; i++)
             {
@@ -112,25 +113,26 @@ namespace gameObjectFactory
                 }
             }
 
-			callBackList2.Clear ();
-		}
+            callBackList2.Clear();
+        }
 
-		public void AddUseNum ()
-		{
-			useNum++;
-		}
+        public void AddUseNum()
+        {
+            useNum++;
+        }
 
-		public void DelUseNum ()
-		{
-			useNum--;
-		}
+        public void DelUseNum()
+        {
+            useNum--;
+        }
 
-		public void Dispose ()
-		{
-			if (data != null) {
+        public void Dispose()
+        {
+            if (data != null)
+            {
 
-				data = null;
-			}
-		}
-	}
+                data = null;
+            }
+        }
+    }
 }
